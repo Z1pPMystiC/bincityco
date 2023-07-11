e4d661bd2d16ba5e594259ba481a299f0d4e4faa
Z1pPMystiC <mishman3004@gmail.com>
2023-07-11 14:13:52 -0500

    FAQ page completed

--- src/components/FAQPage.js
+++ src/components/FAQPage.js
@@ -9,8 +9,53 @@ function FAQPage() {
                 FAQ
             </div>
             <div className='faq-subtitle-text'>
-                The most frequently asked questions and answers
+                Frequently asked questions
             </div>
+        </div>
+        <div className='question-container'>
+          <div className='question-title-text'>
+            What is a bin store?
+          </div>
+          <div className='accent' />
+          <div className='question-text'>
+            A store with bins blyat
+          </div>
+        </div>
+        <div className='question-container'>
+          <div className='question-title-text'>
+            Can I open boxes?
+          </div>
+          <div className='accent' />
+          <div className='question-text'>
+            Absolutely not blyat
+          </div>
+        </div>
+        <div className='question-container'>
+          <div className='question-title-text'>
+            What type of payment do you accept?
+          </div>
+          <div className='accent' />
+          <div className='question-text'>
+            United States Dollars blyat what you want to pay with lev dolbayo?
+          </div>
+        </div>
+        <div className='question-container'>
+          <div className='question-title-text'>
+            What time should I arrive?
+          </div>
+          <div className='accent' />
+          <div className='question-text'>
+            When we're open blyat, what u gonna break in?
+          </div>
+        </div>
+        <div className='question-container'>
+          <div className='question-title-text'>
+            Do you provide bags/carts to shop with?
+          </div>
+          <div className='accent' />
+          <div className='question-text'>
+            Do you have hands blyat? You can carry shit you'll be aight
+          </div>
         </div>
     </div>
   )
